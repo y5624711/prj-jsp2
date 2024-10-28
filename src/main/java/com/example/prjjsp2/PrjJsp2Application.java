7f49d1d6a06452f6903ac2c9128adbe908309a60
Yun <y5624711@gmail.com>
2024-10-28 12:21:25 +0900

    first commit

---
+++ src/main/java/com/example/prjjsp2/PrjJsp2Application.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjJsp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjJsp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prjjsp2/PrjJsp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJsp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
